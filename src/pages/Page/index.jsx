179772f29e8f8ad0a96476327c2ee669eb343e13
jcommaret@gmail.com <jcommaret@gmail.com>
2023-03-22 19:51:07 +0100

    Page and public

--- src/pages/Page/index.jsx
+++ src/pages/Page/index.jsx
@@ -6,6 +6,7 @@ export default function PageComponent({content}){
     <>
     <Helmet>
       <title>{content.yoast_head_json.title}</title>
+      <meta name="description" content={content.title.rendered}/>
     </Helmet>
     <div className="page" id={content.slug}>
       <h1 dangerouslySetInnerHTML={{__html: content.title.rendered}}></h1>
